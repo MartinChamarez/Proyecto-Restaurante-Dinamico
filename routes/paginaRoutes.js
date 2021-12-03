bcf14a035afd6f216417831159e3e4b630a8024e
MartinChamarez <mdc_11@live.com.ar>
2021-12-03 10:04:06 -0300

    .

--- routes/paginaRoutes.js
+++ routes/paginaRoutes.js
@@ -16,18 +16,18 @@ const {
 
 routes.get('/',renderHome);
 
-routes.get('./menu/bebidas',renderBebidas);
+routes.get('/menu/bebidas',renderBebidas);
 
-routes.get("./menu/carnes", renderCarnes);
+routes.get("/menu/carnes", renderCarnes);
 
-routes.get("./menu/hamburguesa", renderHamburguesa);
+routes.get("/menu/hamburguesa", renderHamburguesa);
 
-routes.get("./menu/pastas", renderPastas);
+routes.get("/menu/pastas", renderPastas);
 
-routes.get("./menu/pizza", renderPizza);
+routes.get("/menu/pizza", renderPizza);
 
-routes.get("./menu/sandwich", renderSandwich);
+routes.get("/menu/sandwich", renderSandwich);
 
-routes.get("./menu", renderMenu);
+routes.get("/menu", renderMenu);
 
 module.exports = routes;
